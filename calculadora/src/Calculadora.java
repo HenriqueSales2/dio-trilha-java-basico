d231cb5a3cf7854c4278946a31bfd4a78c9941e8
Henrique Sales <uchihaitachi19112007@gmail.com>
2025-03-10 10:16:24 -0300

    commit de um exemplo de como usar Documentação na linguagem JAVA

---
+++ calculadora/src/Calculadora.java
@@ -0,0 +1,34 @@
+public class Calculadora {
+
+    /** 
+        * @author Henrique Sales
+        * @version 1.0
+        * @since 10/03/2025 
+        */
+
+        /**
+         * este metodo vai somar, subtrair, multiplicar ou dividir
+         * dois números
+         * @param numero1 este é o primeiro parâmetro do método
+         * @param numero2 este é o segundo parâmetro do método
+         * @return int o resultado deste método é a soma dos dois números
+        */
+    
+    public int somar(int numero1, int numero2){
+        return numero1 + numero2;
+    }
+
+    public int subtrair(int numero1, int numero2){
+        return numero1 - numero2;
+    }
+
+    public int multiplicar(int numero1, int numero2){
+        return numero1 * numero2;
+    }
+
+    public int dividir(int numero1, int numero2){
+        return numero1 / numero2;
+    }
+    
+    }
+
---
+++ calculadora/src/Comentarios.java
@@ -0,0 +1,13 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        // Olá eu sou um comentario em uma única linha!!
+
+        /*
+        comentario mais robusto!!
+         */
+
+        /**
+         * comentario serve para mencionar documentação!!
+         */
+    }
+}
